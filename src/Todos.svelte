a3cef05e46c7e5f83b1c346b4354dca987a1fe4e
laverock <they.all.alike@gmail.com>
2020-01-04 18:05:08 +0300

    todos: add handlers, logic and styling

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -5,6 +5,12 @@
   let currentFilter = 'all';
   let nextId = 4;
 
+  $: todosRemaining = filteredTodos.filter(todo => !todo.completed).length;
+  $: filteredTodos = currentFilter === 'all'
+    ? todos : currentFilter === 'completed'
+      ? todos.filter(todo => todo.completed)
+      : todos.filtet(todo => !todo.completed)
+
   let todos = [
     {
       id: 1,
@@ -22,22 +28,78 @@
       completed: false
     },
   ];
+
+  function addTodo(event) {
+    if (event.key === 'Enter') {
+      todos = [...todos, {
+        id: nextId,
+        completed: false,
+        title: newTodoTitle
+      }];
+
+      nextId = nextId + 1;
+      newTodoTitle = '';
+    }
+  }
+
+  function checkAllTodos(event) {
+    todos.forEach(todo => todo.completed = event.target.checked);
+    todos = todos;
+  }
+
+  function updateFilter(newFilter) {
+    currentFilter = newFilter;
+  }
+
+  function clearCompleted() {
+    todos = todos.filter(todo => !todo.completed);
+  }
+
+  function handleDeleteTodo(event) {
+    todos = todos.filter(todo => todo.id !== event.detail.id);
+  }
+
+  function handleToggleComplete(event) {
+    const todoIndex = todos.findIndex(todo => todo.id === event.detail.id);
+    const updatedTodo = {
+      ...todos[todoIndex],
+      completed: !todos[todoIndex].completed
+    };
+    todos = [
+      ...todos.slice(0, todoIndex),
+      updatedTodo,
+      ...todos.slice(todoIndex + 1),
+    ];
+  }
 </script>
 
 <div class="container">
   <h2>Svelte Todo App</h2>
-  <input type="text" class="todo-input" placeholder="Insert todo item ..." bind:value={newTodoTitle} on:keydown={addTodo}>
+  <input
+    type="text"
+    class="todo-input"
+    placeholder="Insert todo item ..."
+    bind:value={newTodoTitle} on:keydown={addTodo}
+  >
 
   {#each filteredTodos as todo}
     <div class="todo-item">
-      <TodoItem {...todo} on:deleteTodo={handleDeleteTodo} on:toggleComplete={handleToggleComplete} />
+      <TodoItem
+        {...todo}
+        on:deleteTodo={handleDeleteTodo}
+        on:toggleComplete={handleToggleComplete}
+      />
     </div>
   {/each}
 
   <div class="inner-container">
     <div>
       <label>
-        <input type="checkbox" class="innet-container-input" on:change={checkAllTodos}>Check All
+        <input
+          type="checkbox"
+          class="inner-container-input"
+          on:change={checkAllTodos}
+        >Check All
       </label>
     </div>
     <div>{todosRemaining} items left</div>
@@ -64,3 +126,46 @@
     </div>
   </div>
 </div>
+
+<style>
+  .container {
+    max-width: 800px;
+    margin: 10px auto;
+  }
+  .todo-input {
+    width: 100%;
+    padding: 10px, 20px;
+    font-size: 18px;
+    margin-bottom: 20px;
+  }
+  .inner-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top: 1px solid lightgrey;
+    padding-top: 15px;
+    margin-bottom: 13px;
+  }
+  .inner-container-input {
+    margin-right: 12px;
+  }
+
+  button {
+    font-size: 14px;
+    background-color: white;
+    appearance: none;
+  }
+
+  button:hover {
+    background: lightseagreen;
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  .active {
+    background: lightseagreen;
+  }
+</style>
